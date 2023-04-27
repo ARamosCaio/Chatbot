7f126f3a0cbd6b484cd4ed02212bff8c1e7c8ef3
Caio Ramos <ARamosCaio@.gmail.com>
2023-04-27 20:18:38 -0300

    added quote

--- main.py
+++ main.py
@@ -11,6 +11,16 @@ def bot():
     msg_response = response.message()
     responded = False
 
+    if 'citação' in received_msg:
+        get_quote = requests.get('https://api.quotable.io/random')
+        if get_quote.status_code == 200:
+            data = get_quote.json()
+            quote = f'{data["content"]} ({data["author"]})'
+        else:
+            quote = 'Não consegui recuperar uma citação neste momento, desculpe.'
+        msg_response.body(quote)
+        responded = True
+
     if 'pato' in received_msg or 'pata' in received_msg:
         msg_response("https://tenor.com/pt-BR/view/cat-catdriving-gif-22785638")
         responded = True
