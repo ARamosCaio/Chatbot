bf387f20af131f25b3f6bd3ba1be7a3167bc0c58
Caio Ramos <aramoscaio@gmail.com>
2023-04-26 21:32:54 -0300

    Added only ducks chatbot

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
-from flask import Flask
+from flask import Flask, request
+import requests
+from twilio.twiml.messaging_response import MessagingResponse
 
 app = Flask(__name__)
 
 @app.route("/bot", methods=['"POST'])
 def bot():
-    pass
+    received_msg = request.values.get('Body', '').lower()
+    response = MessagingResponse()
+    msg_response = response.message()
+    responded = False
+
+    if 'pato' in received_msg or 'pata' in received_msg:
+        msg_response("https://www.pexels.com/search/duck/")
+        responded = True
+
+    if not responded:
+        msg_response.body('Apenas patos')
+    return str(response)
 
 
 if __name__ == "__main__":
